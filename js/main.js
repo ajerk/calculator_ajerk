31148fc247b2a7ede8582634922c3039cb133e48
ajerk <zhuangruijian@qq.com>
2016-06-13 22:08:13 +0800

    second

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-<!--变量-->
-
+//变量
             var operation = new Array([100]); 
             var num = new Array([100]); 
-            for(i = 0 ; i < 100 ; i++){
-                operation[i] = null;
-                num[i] = null;
-            }
             var apoint = null;
             var result = null;
             var screen_result = 0; 
@@ -16,16 +11,23 @@
             var j = 0;
             var a = 0;
             var stop = 1;
+            for(i = 0 ; i < 100 ; i++){
+                operation[i] = null;
+                num[i] = null;
+            }
+            
 
- <!--数字按键-->   
+ //数字按键  
     function number(n){           
-                    if(apoint)                                             <!--num[step]有小数点-->
+                    if(apoint)                                             //num[step]有小数点
                             num[step]=num[step].toString()+n;              
-                    else{                                                  <!--num[step]没有小数点-->
+                    else{                                                  //num[step]没有小数点
                         if(num[step] == null)
-                            num[step]=parseInt(num[step]+n);
-                        else
-                            num[step]=parseInt(num[step].toString()+n);     <!--避免出现输入类似“00001”或者"000002"的数字-->
+                            num[step]=(parseInt(num[step]+n)).toString();
+                        else if(parseFloat(num[step]) == 0)
+                            num[step]=(parseInt(num[step].toString()+n)).toString();     //“00001”或者"000002"的数字
+                        else 
+                            num[step]=(num[step].toString()+n).toString();     
                         }
                     if(step == 0)                                           
                         screen_result = num[step];
@@ -46,16 +48,20 @@
                         screenshow(screen_result);                
     }
        
-<!--小数点按键--> 
+//小数点按键
     function point(){
-            if(!apoint){                                                      <!--该数字还没有小数点时才能加小数点-->
-                apoint = ".";                                                 <!--point 赋值-->
-                    if(num[step])                                             <!--num[step]不为 0 或 null -->
-                        num[step] = num[step].toString() + ".";
-                    else                                                      <!--num[step] 为 0 或 null -->
-                        num[step] = 0 + ".";
+            if(!apoint){                                                      //该数字还没有小数点时才能加小数点
+                apoint = ".";                                                 //point 赋值
+                if(num[step])                                             //num[step]不为 0 或 null 
+                    num[step] = num[step].toString() + ".";
+                else if( num[step] == 0)                                                     //num[step] 为 0 或 null 
+                    num[step] = 0 + ".";
+                else if(step == 0)
+                    num[step] = 0 + ".";
+                else 
+                    num[step] = ".";
                 }                
-            if(step == 0)                                           <!--step == 0 时输出没有小数点-->
+            if(step == 0)                                         
                         screen_result = num[step];
             else for( i = 0 ,screen_result = null ; i < step+1  ; i++ ){                              
                             if(!operation[i])
@@ -73,20 +79,20 @@
     }
     
  
-<!--运算符按键-->
+//运算符按键
     function operation_(n){
-        if(step == 0){                              <!--step为0-->
-            if(!equa){                             <!--之前没有执行=号-->
-                if( !num[step] ){                   <!--num[step]==null或者0-->
+        if(step == 0){                              //step为0
+            if(!equa){                             //之前没有执行=号
+                if( !num[step] ){                   //num[step]==null或者0
                     num[step] = 0 ;
                 }
             }
-            else if(num[step] == null){             <!--之前有执行=号,且num[step]为null时-->
-                num[step] = result;                 <!--将=号执行玩完后的结果赋值给num[step]-->   
+            else if(num[step] == null){             //之前有执行=号,且num[step]为null时
+                num[step] = result;                 //将=号执行玩完后的结果赋值给num[step] 
             }
-            operation[step] = n;                    <!--operation赋值n-->      
+            operation[step] = n;                    //operation赋值n   
         }
-        else if(num[step] !==null){                 <!--当之前没有执行=号-->
+        else if(num[step] !==null){                 //当之前没有执行=号
             operation[step] = n ;
         }
         else 
@@ -103,27 +109,42 @@
         equa = null;
         apoint = null;
     }
-
-<!--0不能被除的提示-->  
-    function warn_zero(){
-        alert("0不能做除数");        
-    }
         
-<!--等于号按键-->   
+//等于号按键   
     function equal(){ 
         if(!equa){
-            if(step >0 && num[step] == null){           <!--在执行 = 号之前，最后一个单位是运算符（ + - * ÷ ）而不是数字的时候把该运算符去掉-->
-                operation[step-1] == null;
-                step--;
-            }
+            stop = 1;
             for(i = 0 ; i < step ; i++){
-                if(operation[step-i-1] == "÷"){
+                if(operation[step-i-1] == "÷"){                     //  "÷"号检查
                     if(parseFloat(num[step-i-1+1]) == 0)
-                        stop = 0;
+                        stop = 2;
+                }
+            }
+            for(i = 0 ; i < (num[0].toString()).length ; i++){     //处理当num【0】的绝对值值很大、用"**e***"的科学计数法表示时可能发生的异常
+                if((num[0].toString()).substring(i,i+1) == "e"){             
+                    if((num[0].toString()).length == i+1){        //  "**e"的情况
+                        stop = 3;
+                    }
+                    else                                         //  "**e**.***"的情况
+                        for(j = i+1 ; j < (num[0].toString()).length ; j++){
+                            if((num[0].toString()).substring(j,j+1) == "." ){
+                                stop = 3;
+                            }
+                        }
                 }
             }
             if(stop==1){
-                for(i = 0 ; i < step ; i++){                                                    <!--执行乘法-->                        
+                if(step >0 && num[step] == null){           //在执行 = 号之前，最后一个单位是运算符（ + - * ÷ ）而不是数字的时候把该运算符去掉
+                    operation[step-1] == null;
+                    step--;
+                }   
+                for(i = 0 ; i < step+1 ; i++){                                  //将num[i]值为".***"的改为"0.***"
+                if(num[i] !== null){
+                    if((num[i].toString()).substring(0,1) == ".")
+                        num[i] = "0" + num[i];
+                    }
+                }          
+                for(i = 0 ; i < step ; i++){                                                    //执行乘法                       
                     if(operation[i] == "×"){
                         if( parseInt(num[i]) == parseFloat(num[i]) &&                    
                             parseInt(num[i+1]) == parseFloat(num[i+1]) )   
@@ -138,10 +159,12 @@
                             num[i+2+j] = null;
                         }
                         i--;
-                        if(step == i+2)
+                        if(step == i+2){
+                            i++;
                             break;
+                        }
                     }
-                    if(operation[i] == "÷"){                                                    <!--执行除法-->
+                    if(operation[i] == "÷"){                                                    //执行除法
                         if( parseInt(num[i]) == parseFloat(num[i]) &&                    
                             parseInt(num[i+1]) == parseFloat(num[i+1]) )   
                           num[i] = parseInt(num[i]) / parseInt(num[i+1]);
@@ -155,14 +178,16 @@
                             num[i+2+j] = null;
                         }
                         i--;
-                        if(step == i+2)
-                            break;                   
+                        if(step == i+2){
+                            i++;
+                            break;
+                        }                  
                     } 
                 }
                 
 
                 
-                for(i = 0 ; i < step ; i++){                                                    <!--执行加法-->                        
+                for(i = 0 ; i < step ; i++){                                                    //执行加法                      
                     if(operation[i] == "+"){
                         if( parseInt(num[i]) == parseFloat(num[i]) &&                   
                             parseInt(num[i+1]) == parseFloat(num[i+1]) )   
@@ -177,10 +202,12 @@
                             num[i+2+j] = null;
                         }
                         i--;
-                        if(step == i+2)
+                        if(step == i+2){
+                            i++;
                             break;
+                        }
                     }
-                    if(operation[i] == "-"){                                                    <!--执行减法-->
+                    if(operation[i] == "-"){                                                    //执行减法
                         if( parseInt(num[i]) == parseFloat(num[i]) &&                    
                             parseInt(num[i+1]) == parseFloat(num[i+1]) )   
                           num[i] = parseInt(num[i]) - parseInt(num[i+1]);
@@ -194,8 +221,10 @@
                             num[i+2+j] = null;
                         }
                         i--;
-                        if(step == i+2)
-                            break;                    
+                        if(step == i+2){
+                            i++;
+                            break;
+                        }                    
                     } 
                 }
                             
@@ -205,6 +234,10 @@
                         result = parseInt(num[0]);
                     else
                         result = parseFloat(num[0]);
+                    for(i = 0 ; i < (result.toString()).length ; i++)               // 科学计数法
+                        if((result.toString()).substring(i,i+1) == "+"){              
+                            result = (result.toString()).substring(0,i) + (result.toString()).substring( i+1 , (result.toString()).length);
+                        }
                     screen_result = result; 
                     screenshow(screen_result);
                     step = 0;
@@ -217,13 +250,15 @@
                     equa = "=";
                 }
             }
-            else
-                warn_zero();
+            else if(stop == 2)
+               alert("0不能做除数");
+            else if(stop == 3)
+               alert(num[0]+"  出错啦");
         }       
     }
         
         
-<!--归零按键-->
+//归零按键
     function clean(){
          for(i = 0 ; i < 100 ; i++)
          {
@@ -244,38 +279,47 @@
     
     
     
-<!--退格--> 
+//退格 
     function back(){
-        if(step == 0){                                                                      <!--step == 0 时，可能是刚执行完 = 号，也可能是刚输入第一位数num[0] -->  
-            if( num[step] == null )                                                         <!--刚执行完 = 号-->
+        if(step == 0){                                                                      //step == 0 时，可能是刚执行完 = 号，也可能是刚输入第一位数num[0]  
+            if( num[step] == null )                                                         //刚执行完 = 号
                 clean();                                                        
-            else if((num[step].toString()).length == 1)                                     <!--num[0]只有一位数而已，退格将变为0-->
-                num[step] = 0;
+            else if((num[step].toString()).length == 1){                                     //num[0]只有一位数而已，退格将变为0
+                if(num[step] == "."){
+                    num[step] = null;
+                    apoint = null;
+                }
+                else 
+                    num[step] = 0;
+            }
             else if((num[step].toString()).substring((num[step].toString()).length-1) == "."){                       
                 num[step] = (num[step].toString()).substring(0,(num[step].toString()).length-1); 
-                apoint = null;                                                                      <!--删除掉apoint-->                           
+                apoint = null;                                                                      //删除掉apoint                           
             }
             else
-                num[step] = (num[step].toString()).substring(0,(num[step].toString()).length-1);    <!--num[0]超过一位数-->
+                num[step] = (num[step].toString()).substring(0,(num[step].toString()).length-1);    //num[0]超过一位数
         }
-        else {                                                                                      <!-- step > 0 , 即是有至少一个运算符（ + - * ÷ ）的时候-->
+        else {                                                                                      // step > 0 , 即是有至少一个运算符（ + - * ÷ ）的时候
             if(num[step] !== null){
-                if((num[step].toString()).length == 1)                        <!--此时step>0 ，num[step]不是第一个数，且当num[step]只有一位数，退格将变为null-->
+                if((num[step].toString()).length == 1){                        //此时step>0 ，num[step]不是第一个数，且当num[step]只有一位数，退格将变为null
+                    if(num[step] == ".")
+                        apoint = null;
                     num[step] = null;
-                else if((num[step].toString()).substring((num[step].toString()).length-1) == ".") <!--num[step]不只一位数-->     
+                }
+                else if((num[step].toString()).substring((num[step].toString()).length-1) == ".") //num[step]不只一位数     
                     {
                      num[step] = (num[step].toString()).substring(0,(num[step].toString()).length-1); 
-                     apoint = null;                                                               <!--删除掉apoint--> 
+                     apoint = null;                                                               //删除掉apoint 
                     }
                 else
                     num[step] = (num[step].toString()).substring(0,(num[step].toString()).length-1); 
             }
-            else {                                                                             <!--num[step]为null ， 退格将删除掉运算符 operation[step-1]-->
+            else {                                                                             //num[step]为null ， 退格将删除掉运算符 operation[step-1
                 operation[step-1] = null;
                 step--;
             }       
         }                                                
-            for( i = 0 ,screen_result = null ; i < step+1  ; i++ ){                            <!--输出过程中防止 "字符串" + null 导致输出异常的情况--> 
+            for( i = 0 ,screen_result = null ; i < step+1  ; i++ ){                            //输出过程中防止 "字符串" + null 导致输出异常的情况 
                 if(!operation[i]){
                     if (screen_result == null )
                         screen_result = num[i];
@@ -293,7 +337,7 @@
     
     
     
-<!--屏幕显示-->
+//屏幕显示
     function screenshow(n){
         show = n.toString();
         if(show.length < 9 || show.length == 9  ){ 
